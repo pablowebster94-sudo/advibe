706645ae1496919886aaa6f10fcfbbe949b9a043
pablowebster94-sudo <pablowebster94@gmail.com>
2026-09-21 02:20:56 -0500

    fix: isolate Meta Pixel by site host

--- components/MetaPixel.tsx
+++ components/MetaPixel.tsx
@@ -2,28 +2,25 @@
 
 import Script from "next/script";
 
-const pixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID;
-
 export default function MetaPixel() {
-  if (!pixelId || typeof window === "undefined") return null;
-  const host = window.location.hostname;
   const isEnfoque =
-    host === "enfoque.advibeagencia.com" ||
-    host === "enfoquevisual.advibeagencia.com";
-  if (isEnfoque) return null;
+    typeof window !== "undefined" &&
+    (window.location.hostname === "enfoque.advibeagencia.com" ||
+      window.location.hostname === "enfoquevisual.advibeagencia.com");
 
-  return (
-    <Script id="meta-pixel" strategy="afterInteractive">
-      {`!function(f,b,e,v,n,t,s)
-      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-      n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)}(window, document,'script',
-      'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '${pixelId}');
-      fbq('track', 'PageView');`}
-    </Script>
-  );
+  const pixelId = isEnfoque
+    ? process.env.NEXT_PUBLIC_ENFOQUE_META_PIXEL_ID
+    : process.env.NEXT_PUBLIC_META_PIXEL_ID;
+
+  if (!pixelId) return null;
+
+  const code =
+    "!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?" +
+    "n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;" +
+    "n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;" +
+    "t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}" +
+    "(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');" +
+    "fbq('init','" + pixelId + "');fbq('track','PageView');";
+
+  return <Script id="meta-pixel" strategy="afterInteractive">{code}</Script>;
 }
