cec510d18efd32ce30ef904f8307aa72d7255615
pablowebster94-sudo <pablowebster94@gmail.com>
2026-09-21 02:22:46 -0500

    fix: avoid duplicate Enfoque Meta Pixel initialization

--- app/enfoque-visual/layout.tsx
+++ app/enfoque-visual/layout.tsx
@@ -9,19 +9,8 @@ export const metadata: Metadata = {
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const pixel = process.env.NEXT_PUBLIC_ENFOQUE_META_PIXEL_ID;
   const ga = process.env.NEXT_PUBLIC_ENFOQUE_GA_ID || process.env.NEXT_PUBLIC_GA_ID;
-  const pixelCode = pixel
-    ? "!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?" +
-      "n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;" +
-      "n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;" +
-      "t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}" +
-      "(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');" +
-      "fbq('init','" + pixel + "');fbq('track','PageView');"
-    : "";
-
   return <>
-    {pixel && <Script id="ev-pixel" strategy="afterInteractive">{pixelCode}</Script>}
     {ga && <>
       <Script src={"https://www.googletagmanager.com/gtag/js?id=" + ga} strategy="afterInteractive" />
       <Script id="ev-ga" strategy="afterInteractive">
